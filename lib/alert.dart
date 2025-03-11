5181635b8b3211ebdbe6747700cc8f21949b305f
R482495 <136292210+R482495@users.noreply.github.com>
2025-03-12 02:37:07 +0900

    Update alert.dart

--- lib/alert.dart
+++ lib/alert.dart
@@ -1,30 +1,21 @@
+
 import 'package:flutter/material.dart';
 
-// Alert クラスを定義
 class Alert {
-  // 静的メソッド showAlert を定義
-  // このメソッドはエラーダイアログを表示する
   static void showAlert(BuildContext context, String text) {
-    // showDialog メソッドを使用してダイアログを表示
-    showDialog(
-      context: context, // ダイアログを表示するためのビルドコンテキストを指定
-      builder: (_) {
-        // AlertDialog ウィジェットを返す
-        return AlertDialog(
-          title: const Text("Error"), // ダイアログのタイトルを設定
-          content: Text(text), // ダイアログの内容を設定
-          actions: [
-            // アクションボタンを設定
-            TextButton(
-              child: const Text('OK'), // ボタンのテキストを設定
-              onPressed: () {
-                // ボタンが押された時の処理を設定
-                Navigator.pop(context); // ダイアログを閉じる
-              },
-            )
-          ],
-        );
-      },
-    );
+    showDialog(context: context,builder: (_) {
+      return AlertDialog(
+        title: const Text("Error"),
+        content: Text(text),
+        actions: [
+          TextButton(
+            child: const Text('OK'),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          )
+        ],
+      );
+    });
   }
 }
